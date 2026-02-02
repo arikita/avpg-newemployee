caa926e1b25775f0f8d978ff5b35405173dce5a9
arikita <arikjta@gmail.com>
2026-02-02 18:33:32 +0700

    Up backend to cloud

--- backend/server.js
+++ backend/server.js
@@ -124,4 +124,5 @@ app.post("/generate", upload.single("image"), async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("🚀 Server chạy tại http://0.0.0.0:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server chạy tại port ${PORT}`));
